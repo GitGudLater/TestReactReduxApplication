cb2364e5641e7248184875197c119c2afb88395e
Andrey <slendernet@@gmail.com>
2020-03-13 12:26:28 +0300

    Added simple pagination

--- testreactapp/src/components/ReposDashboard.jsx
+++ testreactapp/src/components/ReposDashboard.jsx
@@ -9,12 +9,16 @@ export default class ReposDashboard extends React.Component {
         this.state = {
             globalRepositoriesList: null,
             isLoaded: false,
+            page: 1
         }
+        this.incrementPageFunction = this.incrementPageFunction.bind(this);
+        this.decrementPageFunction = this.decrementPageFunction.bind(this);
+        this.callReposPage = this.callReposPage.bind(this);
     }
 
     componentDidMount(){
 
-        axios.get(`https://api.github.com/search/repositories?q=stars:>=500&sort=stars&order=desc`)
+        axios.get(`https://api.github.com/search/repositories?q=stars:>=500&sort=stars&order=desc&per_page=5&page=1`)
         .then(
             result => {
                 this.setState(
@@ -27,8 +31,37 @@ export default class ReposDashboard extends React.Component {
         );
     }
 
+    callReposPage() {
+        const {page} = this.state;
+        axios.get(`https://api.github.com/search/repositories?q=stars:>=500&sort=stars&order=desc&per_page=5&page=${page}`)
+        .then(
+            result => {
+                this.setState(
+                    {
+                        globalRepositoriesList: result.data,
+                        isLoaded:true
+                    }
+                )
+            }
+        );
+    }
+
+    incrementPageFunction(e){
+        e.preventDefault();
+        const currentPage = this.state.page;
+        this.setState({page: currentPage+1});
+        this.callReposPage();
+    }
+
+    decrementPageFunction(e){
+        e.preventDefault();
+        const currentPage = this.state.page;
+        this.setState({page: currentPage-1});
+        this.callReposPage();
+    }
+
     render() {
-        const {globalRepositoriesList,isLoaded} = this.state;
+        const {globalRepositoriesList,isLoaded,page} = this.state;
         const globalList = isLoaded ? 
         <ul type="none" className="topTierList">
             {globalRepositoriesList.items.map(item => (
@@ -39,9 +72,11 @@ export default class ReposDashboard extends React.Component {
         </ul> : "loading..." ;
         return(
             <div className="topTierRepositoriesContainer">
-            Top tier of repositories with highest numbers of stars
-            {globalList}
-        </div>
+                Top tier of repositories with highest numbers of stars
+                <button style={{display: page > 1 ? "inline" : "none" }} className="previousPageBtn" onClick={(event) => this.decrementPageFunction(event)}>Previous page</button>
+                <button style={{display: globalList == [] ? "none" : "inline" }} className="nextPageBtn" onClick={(event) =>this.incrementPageFunction(event)}>Next page</button>
+                {globalList}
+            </div>
         )
     }
 }
