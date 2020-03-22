92a29f5dc15c42a9e5ee9fe1a7c69d8da533827e
Andrey <slendernet@@gmail.com>
2020-03-22 08:57:09 +0300

    Code cleaning

--- testreactapp/src/index.js
+++ testreactapp/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import './styles.css';
-import * as serviceWorker from './serviceWorker';
 import {Provider,connect} from 'react-redux';
 import {createStore,bindActionCreators,applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
@@ -69,16 +67,3 @@ ReactDOM.render(/*Put store to props*/<Provider store={store}><WrappedAppCompone
 
 
 
-
-
-
-
-
-
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
